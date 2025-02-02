7183d2d844f2b5f6b8fb4c7301e9e4c7b628ae96
ruka <vanilla.0704@icloud.com>
2025-02-02 11:43:30 +0900

    確認用ルーティング

--- rehem-app/app/Http/Controllers/AccountContller.php
+++ rehem-app/app/Http/Controllers/AccountContller.php
@@ -1,27 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Model\AccountContller;
 
 
 class AccountContller extends Controller
 {
-    class mainFanction{
-
-        $account_db = function interface(){
-            $account = AccountContller::all();
-            return redirect()->route("post");
-        }
-
-        // りょうまのとこですよ
-        function post($account_db){
-
-            
-
-        }
-        
+    public function top()
+    {
+        return view("main");
     }
+
     
+    public function profile()
+    {
+        return view("profile");
+    }
+
+    public function record()
+    {
+        return view("record");
+    }
+
 }
