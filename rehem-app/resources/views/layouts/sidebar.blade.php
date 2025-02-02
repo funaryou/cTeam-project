5d96c89c0dfce5a5c6bbbbfb7bdd80824c0b328b
haruominomura <haruomi.nomura@gmail.com>
2025-02-02 18:36:42 +0900

    create profile page

--- rehem-app/resources/views/layouts/sidebar.blade.php
+++ rehem-app/resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <header class="sidebar">
         <ul class="nav">
-            <li class="home navItem"><a href="{{ route('top') }}"></a></li>
+            <li class="{{ Route::currentRouteName() === 'top' ? 'active' : ''}} home navItem"><a href="{{ route('top') }}"></a></li>
             <div class="divider"></div>
-            <li class="addRecord navItem"><a href="{{ route('record') }}"></a></li>
+            <li class="{{ Route::currentRouteName() === 'record' ? 'active' : ''}} addRecord navItem"><a href="{{ route('record') }}"></a></li>
             <div class="divider"></div>
-            <li class="profile navItem"><a href=""></a></li>
+            <li class="{{ Route::currentRouteName() === 'profile' ? 'active' : ''}} profile navItem"><a href="{{ route('profile') }}"></a></li>
         </ul>
     </header>
